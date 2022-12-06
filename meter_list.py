5e78f2b5bbcc087a037b3ffe48fe3c7ed46505a5
Matteo Zecchetti <mattezeck@gmail.com>
2022-12-06 20:53:16 +0100

    tenth commit

--- meter_list.py
+++ meter_list.py
@@ -8,3 +8,10 @@ for item, row in contatori.iterrows():
     db.collection("lista contatori").document(str(int(row.n_serie))).set({
         "lat": str(row.lat), "long": str(row.long)
     })
+
+# creazione dataframe 'contatori' con ogni contatore e lat e long casuali
+# contatori = pd.DataFrame(
+#    {'n_serie': df["Numero di serie del contatore"].unique()})
+# contatori["lat"] = contatori.apply(lambda row: lat+(0.5-random())/100, axis=1)
+# contatori["long"] = contatori.apply(
+#    lambda row: long+(0.5-random())/100, axis=1)
